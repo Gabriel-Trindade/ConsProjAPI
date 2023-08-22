b1829a5a548202929db905b2531c27e4fa1cca92
Gabriel Trindade <gtrindade@almbrasil.com>
2023-08-22 16:28:14 -0300

    Adicionar arquivos de projeto.

---
+++ ConsProjAWS/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsProjAWS.Models;
+
+public class Categoria
+{
+    public int CategoryId { get; set; }
+
+    public string? CategoryName { get; set; }
+
+    public string? ImageURL { get; set; }
+
+
+}
---
+++ ConsProjAWS/Models/Produto.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsProjAWS.Models;
+
+public class Produto
+{ 
+    public int ProdutoId { get; set; }
+
+    public string? ProdutoName { get; set;}
+
+    public string? Description { get; set;} 
+
+    public decimal Price { get; set; }
+
+    public string? ImageURL { get; set; }
+
+    public float stock { get; set;}
+
+    public DateTime DataCadastro { get; set; }
+
+
+}
